113ed7091c574164656d9331de037c56b95fed64
Adam King <kingadam7@gmail.com>
2019-04-02 08:28:53 -0700

    Assignment work

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -21,12 +21,20 @@ module.exports = class ContactController {
          validate(val){
            return val !== "";
          }
+       },
+       {
+         type: "input",
+         name: "email",
+         message: "Contact's email - ",
+         validate(val){
+           return val !== "";
+         }
        }
      ];
   }
 
   addContact(name, phone){
-     return Contact.create({name, phone})
+     return Contact.create({name, phone, email})
    }
 
 }
